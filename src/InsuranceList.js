34d5b1d628abad436bf04f664cf37e25b7090f9c
moddather rashed <moddather.developer@gmail.com>
2017-11-25 17:25:13 +0000

    getting the first scene ready

---
+++ src/InsuranceList.js
@@ -0,0 +1,16 @@
+import React,{Component} from 'react'
+import {View , Text } from 'react-native'
+
+
+class InsuranceList extends Component {
+
+    render(){
+        return(
+            <View>
+                <Text>hello world from insrauce list</Text>
+            </View>
+        )
+    }
+}
+
+export default InsuranceList
